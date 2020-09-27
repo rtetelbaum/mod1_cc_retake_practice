f6e28834ccd5bcbda93c0c236e57cedd9d8f3fff
rtetelbaum <rtetelbaum@mailbox.org>
2020-09-27 15:54:01 -0400

    initial commit CC started

---
+++ app/models/recipe.rb
@@ -0,0 +1,16 @@
+def Recipe
+	attr_reader :name
+	attr_accessor :description
+
+	@@all = []
+
+	def initialize(name, description)
+		@name = name
+		@description = description
+		@@all << self
+	end
+
+	def self.all
+		@@all
+	end
+end
