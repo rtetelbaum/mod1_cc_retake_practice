ccf734684810d41a620bd4eed9e073ef78ca881f
rtetelbaum <rtetelbaum@mailbox.org>
2020-09-27 16:01:34 -0400

    everything up to ORM

---
+++ app/models/restaurant.rb
@@ -0,0 +1,17 @@
+def Restaurant
+	attr_reader :owner
+	attr_accessor :name, :star_rating
+
+	@@all = []
+
+	def initialize(owner, name, star_rating)
+		@owner = owner
+		@name = name
+		@star_rating = star_rating
+		@@all << self
+	end
+
+	def self.all
+		@@all
+	end
+end
