5022da6cbe9112447781f3d853484d9e5d92fccb
rtetelbaum <rtetelbaum@mailbox.org>
2020-09-29 22:50:21 -0400

    initial models done

---
+++ app/models/menu_item.rb
@@ -0,0 +1,17 @@
+class MenuItem
+	attr_accessor :price
+	attr_reader :restaurant, :recipe, 
+
+	@@all = []
+
+	def initialize(restaurant, recipe, price)
+		@restaurant = restaurant
+		@recipe = recipe
+		@price = price
+		@@all << self
+	end
+
+	def self.all
+		@@all
+	end
+end
